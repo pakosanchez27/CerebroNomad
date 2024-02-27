2919e2b11803652fa482f5685787f97e0fdf8d59
Francisco Javier Sánchez <javier.sanchezjps27@gmail.com>
2024-02-26 20:47:57 -0600

    Add: Rutas para el Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,20 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.scss')
     <title>Document</title>
 </head>
-
 <body>
-    <nav>
-        <a href="{{route('login')}}">Login</a>
-        <a href="#">Nosotros</a>
-        <a href="#">API Documentación</a>
-    </nav>
+    <h1>Hola mundo</h1>
 </body>
-
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
